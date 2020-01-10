ac21fe3f9833e7a18407cf3f1e7551240905cc8f
jennlindley <jennifersu2059@gmail.com>
2020-01-10 16:37:52 -0500

    avance

--- src/vistas/muro.js
+++ src/vistas/muro.js
@@ -1,17 +1,37 @@
 export default () => {
   const vistaMuro = `
-    <section class= 'perfil'>
-      <figure id='figureLogo'>
-        <img src='img/logo.jpeg' id='logoLoginRegistro'>
-      </figure>
-     
-    <section id='container-posts' class='container-posts'>
-    <div class='card-new-post'>
-      <textarea name='' id='' placeholder='¿Qué quieres compartir?'></textarea>
-      <i class='icon-general far fa-images'></i>
-      <button class='share-post type='submit'>Compartir</button>
-    </div>
-  </section>
+    <section class= 'muro'>
+      <header id='barrita'>
+        <nav id='barraHorizontal'>
+          <figure id='figureLogo'>
+            <img src='img/logo.jpeg' id='logoLoginRegistro'>
+          </figure>
+          <input type="checkbox" id="menu-toggle"/>
+  <label id="trigger" for="menu-toggle"></label>
+  <label id="burger" for="menu-toggle"></label>
+  <ul id="menu">
+    <li><a href="#">Home</a></li>
+    <li><a href="#">About</a></li>
+    <li><a href="#">Portfolio</a></li>
+    <li><a href="#">Contact</a></li>
+  </ul>
+        </nav>    
+      </header>  
+        <section class='profile'>
+            <div class='front'>Mariana Costa</div>
+                <div class='userProfile'>
+                    <img src='https://image.flaticon.com/icons/png/512/145/145847.png' id='imgPerfil'>
+                </div>
+            </div>
+        </section>
+      
+        <section id='container-posts' class='container-posts'>
+            <div class='card-new-post'>
+                <textarea name='' id='' placeholder='¿Qué quieres compartir?'></textarea>
+                <i class="fas fa-image"></i> 
+                <button class='share-post type='submit'>Compartir</button>
+            </div>
+        </section>
 
     </section>
 `;
