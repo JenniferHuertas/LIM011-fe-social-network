d9677e06b298bbc42a635f0a559d6b0e3bfca7d6
jennlindley <jennifersu2059@gmail.com>
2020-01-20 15:51:47 -0500

    ruteo arreglado

--- src/utils/util-views.js
+++ src/utils/util-views.js
@@ -1,11 +1,11 @@
-import viewRegister from '../vistas/registro-vistas.js';
-import viewLogin from '../vistas/login-vistas.js';
+import { divVistaRegistro } from '../vistas/registro-vistas.js';
+import { divVistaLogin } from '../vistas/login-vistas.js';
 import viewHome from '../vistas/home-vistas.js';
 import Different from '../vistas/404-vistas.js';
 
 export const components = {
-  register: viewRegister,
-  login: viewLogin,
+  createAccount: divVistaRegistro,
+  login: divVistaLogin,
   home: viewHome,
   different: Different,
 
